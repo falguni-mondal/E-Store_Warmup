a6dae9256e2116065ec67af178860851ecb8f459
Falguni Mondal <career.falguni@gmail.com>
2024-09-24 14:29:48 +0530

    Bug fixes related to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
         <div className="flex-shrink-0 nav-container fixed z-[99] lg:static w-screen lg:w-[20%] h-[6vh] lg:h-screen flex lg:block items-center justify-end px-8 lg:px-0 border-b bg-white lg:bg-transparent">
             <span onClick={navRevealer} className='font-black lg:hidden'>Menu</span>
             <TfiClose onClick={navRevealer} className={`absolute z-50 ${reveal ? "block" : "hidden"} lg:hidden text-[1.5rem]`}/>
-            <nav className={`${reveal ? "flex" : "hidden"} lg:${reveal ? "flex" : "flex"} w-full h-screen lg:h-fit absolute top-0 left-0 lg:static p-5 pt-20 lg:pt-8 flex flex-col items-center bg-white text-slate-900 overflow-x-hidden overflow-y-auto text-[1.2rem] lg:text-[1rem]`}>
+            <nav className={`navbar ${reveal ? "flex" : "hidden"} w-full h-screen lg:h-fit absolute top-0 left-0 lg:static p-5 pt-20 lg:pt-8 flex flex-col items-center bg-white text-slate-900 overflow-x-hidden overflow-y-auto text-[1.2rem] lg:text-[1rem]`}>
                 <NavLink onClick={navRevealer} to="/" className='px-8 py-1.5 border border-blue-700 rounded-md shodow bg-blue-600 text-white'>All Products</NavLink>
                 <NavLink onClick={navRevealer} to="/add product" className='px-5 py-1.5 mt-3 border border-black rounded-md shodow text-black'>Add new product</NavLink>
                 <div className='w-full mt-5 pt-3 border-t border-black min-h-[55vh]'>
